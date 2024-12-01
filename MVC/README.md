00712588e4487191aab5036fc0c7488a300fbb41
Thamindu Wijerathne <140165111+Thamindu-wijerathne@users.noreply.github.com>
2024-12-01 13:52:04 +0530

    1.16.27

---
+++ MVC/README.md
@@ -0,0 +1,1 @@
+MVC Architecture using html css php js
---
+++ app/controllers/Products.php
@@ -0,0 +1,11 @@
+product controller<?php 
+
+class Products extends Controller
+{
+    public function index()
+    {
+        echo "This is the products controller";
+
+        $this->view('products');
+    }
+}
